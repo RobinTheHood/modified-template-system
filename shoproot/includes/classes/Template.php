001028f11799ebed849fe9a6591dacad58d3ebf0
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-01-25 10:42:26 +0100

    chore: add method getPath() to Template

--- shoproot/includes/classes/Template.php
+++ shoproot/includes/classes/Template.php
@@ -6,6 +6,11 @@ namespace Modified\Core;
 
 class Template
 {
+    public function getPath(string $templatePath): string
+    {
+        return DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/' . $templatePath;
+    }
+
     public function fetch($smartyObj, string $templatePath)
     {
         return $smartyObj->fetch(CURRENT_TEMPLATE . '/' . $templatePath);
